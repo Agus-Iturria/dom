ecaa230572f1d44e16403de3830f1b3aced93c1f
Agus-Iturria <agustin.iturria@gm2dev.com>
2024-08-22 12:52:56 -0300

    feat(domManipulation.js): add addButtons and create a delete function

--- domManipulation.js
+++ domManipulation.js
@@ -1,70 +1,59 @@
-//DOM Manipulation Exercise
-//Based on Notion ToDo list style, create a web app that contains two lists side by side.
-//1.Each list name must be at the end of the amount of items.
-//2.Load an initial list of ToDos for each list when the page is loaded
-//a.First list
-//i.Buy eggs
-//ii.Do laundry
-//iii.Buy facturas for Seba
-//b.Second list
-//i.Sleep
-//ii.Eat
-//iii.Code
-//iv.Repeat
-//3.The last list item must be an empty item allowing users to add a new item to that list. It must have a button on the right side with the text “Add”
-//4.Allow users to delete items, with a button to the right with the text “Delete”
-//a.Extra: alert users to see if they are sure
-//5.Add a button to toggle light/dark mode.
-//a.Do not duplicate html code.
-//6.Manage variable amount of lists.
-//(Do not copy from Notion, use provided snapshot as reference).
-//Tips
-//Hardcode first. Then generalize and abstract variables.
-//Start small. Then build bigger and adapt along the way
-//Ask for help! But after first trying to do it yourself, and then, understanding what is blocking you (“What is it that I don’t understand?“)
-
 let list1 = document.querySelector("#list1");
 let list2 = document.querySelector("#list2");
 let addButton1 = document.querySelector(".add-btn1");
 let addButton2 = document.querySelector(".add-btn2");
-let input = document.querySelector("input1");
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let toggleButton = document.getElementById("toggle-theme");
 
-let toggleButon = document.getElementById("toggle-theme");
-let deleteButtons = document.querySelectorAll(".delete-btn");
+let todoList1 = ["Buy eggs", "Do laundry", "Buy facturas for Seba", "Hola"];
+let todoList2 = ["Sleep", "Eat", "Code", "Repeat"];
 
-deleteButtons.forEach(function (button) {
-  button.onclick = function () {
-    button.parentElement.remove();
-  };
-});
-
-toggleButon.addEventListener("click", function () {
-  let body = document.body;
-  if (body.classList.contains("dark-mode")) {
-    body.classList.remove("dark-mode");
-    body.classList.add("light-mode");
-  } else {
-    body.classList.remove("light-mode");
-    body.classList.add("dark-mode");
-  }
-});
-
-//addButton1.addEventListener("click", function () {
-//  let newItemText = input.textContent;
-//  let newItem = document.createElement("li");
-//  newItem.textContent = `${newItemText} <button class="delete-btn">Delete</button>`;
-//  list1.append(newItem);
-//});
-addButton1.addEventListener("click", function () {
-  let newItemText = input;
-  let newItem = document.createElement("li");
-
+function createDeleteButton() {
   let deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
+  deleteButton.textContent = "✖";
   deleteButton.classList.add("delete-btn");
+  deleteButton.onclick = function () {
+    if (confirm("¿Quieres borrarlo?")) {
+      this.parentElement.remove();
+    }
+  };
+  return deleteButton;
+}
 
-  newItem.textContent = newItemText;
-  newItem.append(deleteButton);
+function addLists(todolist, list) {
+  for (let i = 0; i < todolist.length; i++) {
+    let newItem = document.createElement("li");
+    newItem.textContent = todolist[i];
+    newItem.append(createDeleteButton());
+    list.append(newItem);
+  }
+}
 
-  list1.append(newItem);
+addLists(todoList1, list1);
+addLists(todoList2, list2);
+
+function addItem(list, input) {
+  let newItemText = input.value;
+  if (newItemText !== "") {
+    let newItem = document.createElement("li");
+    newItem.textContent = newItemText;
+    newItem.append(createDeleteButton());
+    list.append(newItem);
+    input.value = "";
+  }
+}
+
+addButton1.addEventListener("click", function () {
+  addItem(list1, input1);
+});
+
+addButton2.addEventListener("click", function () {
+  addItem(list2, input2);
+});
+
+toggleButton.addEventListener("click", function () {
+  let body = document.body;
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
 });
